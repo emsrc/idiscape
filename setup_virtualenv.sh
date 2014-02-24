672290d2a03d005f4065fed645cab742898504b7
Erwin Marsi <emarsi@idi.ntnu.no>
2014-02-24 13:50:53 +0100

    Install unidecode package with pip instead of scrappy

--- setup_virtualenv.sh
+++ setup_virtualenv.sh
@@ -1,9 +1,10 @@
 # create virtual python enviroment named 'idiscape' using Conda
-conda create -n idi lxml pip ipython twisted lxml six requests scikit-learn pil cython unidecode
+conda create -n idi lxml pip ipython twisted lxml six requests scikit-learn pil cython 
 
 source activate idi
 
 # install latest version of scrapy (conda installs outdated version 0.16)
-pip install scrapy
+pip install scrapy unidecode
+
 
 
